928bdd3d3b2407031382e95c8d0936a6092b4cde
Tan Shuai <7anshuai@gmail.com>
2020-05-15 10:36:21 +0800

    Remove license comments

--- tools/start.js
+++ tools/start.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-/**
- * Copyright © 2016-present Kriasoft.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
 
 import cp from 'child_process';
 import clean from './clean';
